671b8b7a50ba031e14e8389a6e396b2f5df9a3d6
Juan Diego <133695239+juandifers@users.noreply.github.com>
2024-12-04 13:39:52 +0100

    Delete build directory

--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rbelt/Desktop/5th semester/prog2/Task_manager_AI/googletest/googletest
-# Build directory: C:/Users/rbelt/Desktop/5th semester/prog2/Task_manager_AI/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
